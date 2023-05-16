51ceefaefeb5d78ab2e321ae5842c7836d8ed96b
AngAcosta <acostavillaangel23@gmail.com>
2023-05-15 19:07:17 -0600

    Add project files.

---
+++ BL/Usuario.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Usuario
+    {
+        public static ML.Result Add(ML.Usuario usuario)
+        {
+            ML.Result result = new ML.Result();
+
+            try
+            {
+                using (DL.AAcostaPruebaEntities context = new DL.AAcostaPruebaEntities())
+                {
+                    var query = context.UsuarioAdd(usuario.Nombre, usuario.ApellidoPaterno, usuario.ApellidoMaterno, usuario.FechaNacimiento);
+
+                    if (query >= 1)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                        result.Message = "Problema al Insertar";
+                    }
+                    result.Correct = true;
+                }
+            }
+            catch (Exception e)
+            {
+                result.Correct = false;
+                result.Message = e.Message;
+            }
+            return result;
+        }
+
+        public static ML.Result Update(ML.Usuario usuario)
+        {
+            ML.Result result = new ML.Result();
+
+            try
+            {
+                using (DL.AAcostaPruebaEntities context = new DL.AAcostaPruebaEntities())
+                {
+                    var query = context.UsuarioUpdate(usuario.IdUsuario, usuario.Nombre, usuario.ApellidoPaterno, usuario.ApellidoMaterno, usuario.FechaNacimiento);
+
+                    if (query >= 0)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                        result.Message = "No se Actualizo el Usuario";
+                    }
+                    result.Correct = true;
+                }
+            }
+            catch (Exception e)
+            {
+                result.Correct = false;
+                result.Message = e.Message;
+            }
+            return result;
+        }
+
+        public static ML.Result Delete(int IdUsuario)
+        {
+            ML.Result result = new ML.Result();
+
+            try
+            {
+                using (DL.AAcostaPruebaEntities context = new DL.AAcostaPruebaEntities())
+                {
+                    var query = context.UsuarioDelete(IdUsuario);
+
+                    if (query >= 1)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                        result.Message = "No se elimino el usuario";
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                result.Correct = false;
+                result.Message = e.Message;
+            }
+            return result;
+        }
+
+        public static ML.Result GetAll()
+        {
+            ML.Result result = new ML.Result();
+
+            try
+            {
+                using (DL.AAcostaPruebaEntities context = new DL.AAcostaPruebaEntities())
+                {
+                    var query = context.UsuarioGetAll().ToList();
+
+                    if (query != null)
+                    {
+                        result.Objects = new List<object>();
+
+                        foreach (var obj in query)
+                        {
+                            ML.Usuario usuario = new ML.Usuario();
+
+                            usuario.IdUsuario = obj.IdUsuario;
+                            usuario.Nombre = obj.Nombre;
+                            usuario.ApellidoPaterno = obj.ApellidoPaterno;
+                            usuario.ApellidoMaterno = obj.ApellidoMaterno;
+                            usuario.FechaNacimiento = obj.FechaNacimiento.ToString();
+
+                            result.Objects.Add(usuario);
+                        }
+                    }
+                    result.Correct = true;
+                }
+            }
+            catch (Exception e)
+            {
+                result.Correct = false;
+                result.Message = e.Message;
+            }
+            return result;
+        }
+
+        public static ML.Result GetById(int IdUsuario)
+        {
+            ML.Result result = new ML.Result();
+
+            try
+            {
+                using (DL.AAcostaPruebaEntities context = new DL.AAcostaPruebaEntities())
+                {
+                    var query = context.UsuarioGetById(IdUsuario).FirstOrDefault();
+
+                    if (query != null)
+                    {
+                        ML.Usuario usuario = new ML.Usuario();
+                        
+                        usuario.IdUsuario = query.IdUsuario;
+                        usuario.Nombre = query.Nombre;
+                        usuario.ApellidoPaterno = query.ApellidoPaterno;
+                        usuario.ApellidoMaterno = query.ApellidoMaterno;
+                        usuario.FechaNacimiento = query.FechaNacimiento.ToString();
+                        
+                        result.Object = usuario;
+                    }
+                    result.Correct = true;
+                }
+            }
+            catch (Exception e)
+            {
+                result.Correct=false;
+                result.Message = e.Message;
+            }
+            return result;
+        }
+    }
+}
---
+++ PL/Controllers/UsuarioController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PL.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            ML.Usuario usuario = new ML.Usuario();
+            ML.Result result = BL.Usuario.GetAll();
+
+            if (result.Correct)
+            {
+                usuario.Usuarios = result.Objects;
+                return View(usuario);
+            }
+            else
+            {
+                return View(usuario);
+            }
+            return View();
+        }
+
+        
+        public ActionResult Form(int? IdUsuario)
+        {
+            ML.Usuario usuario = new ML.Usuario();
+
+            if (IdUsuario == null)
+            {
+                return View(usuario);
+            }
+            else
+            {
+                ML.Result result = BL.Usuario.GetById(IdUsuario.Value);
+
+                if (result.Correct)
+                {
+                    usuario = (ML.Usuario)result.Object;
+
+                    return View(usuario);
+                }
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Form(ML.Usuario usuario)
+        {
+            ML.Result result = new ML.Result();
+
+            if (usuario.IdUsuario == 0)
+            {
+                result = BL.Usuario.Add(usuario);
+
+                if (result.Correct)
+                {
+                    ViewBag.Message = "Usuario Agregado";
+                    return View("Modal");
+                }
+            }
+            else
+            {
+                result = BL.Usuario.Update(usuario);
+
+                if (result.Correct)
+                {
+                    ViewBag.Message = "Usuario Modificado";
+                    return View("Modal");
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Delete(int IdUsuario)
+        {
+            ML.Result result = BL.Usuario.Delete(IdUsuario);
+
+            if (result.Correct)
+            {
+                return View("Modal");
+            }
+            return View();
+        }
+    }
+}
